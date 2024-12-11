382b29324a74c4315981e73b45e8c00ddcc04ff6
whitehat57 <86547977+whitehat57@users.noreply.github.com>
2024-12-11 13:48:27 +0700

    Update squiz-api.py

--- squiz-api.py
+++ squiz-api.py
@@ -202,7 +202,7 @@ def main():
     
     try:
         tester = SquizMatrixTester()
-        tester.flood(target_url, workers=20, duration=120)
+        tester.flood(target_url, workers=30, duration=7200)
     except KeyboardInterrupt:
         print("\n[INFO] Test interrupted by user.")
     except Exception as e:
